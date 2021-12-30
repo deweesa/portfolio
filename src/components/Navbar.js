af36a51baef9a71a691ea1f5ff9df8f036d9bc48
Asa DeWeese <asadeweese@gmail.com>
2021-12-30 13:13:20 -0800

    Removing dead code block

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,25 +29,4 @@ export default function Navbar() {
       </ul>
     </nav>
   );
-  /*return (
-    <nav>
-      <ul className="flex flex-row justify-end my-3">
-        <li>
-          <Link to="/" activeStyle={activeStyle} className={navLinkItem}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className={navLinkItem}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/blog" className={navLinkItem}>
-            Blog
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );*/
 }
